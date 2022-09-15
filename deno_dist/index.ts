78c43e13c09acb46bb8662cef2878bb980fba1e7
garronej <joseph.garrone.gj@gmail.com>
2022-09-15 16:12:08 +0200

    Untrack deno_dist

--- deno_dist/index.ts
+++
@@ -1,5 +0,0 @@
-import * as Pattern from './patterns.ts';
-
-export { match } from './match.ts';
-export { isMatching } from './is-matching.ts';
-export { Pattern, Pattern as P };
--- deno_dist/types/DeepExclude.ts
+++
@@ -1,3 +0,0 @@
-import { DistributeMatchingUnions } from './DistributeUnions.ts';
-
-export type DeepExclude<a, b> = Exclude<DistributeMatchingUnions<a, b>, b>;
